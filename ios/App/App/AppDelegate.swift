3e528b4b4d9ddbbe27398747b150ebc1bc5a06a5
Kausthubh Sukhesha Adhikari <kausthubhadhikari@gmail.com>
2026-02-26 02:28:16 +0530

    Some changes to resolve the orientation issue

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -11,6 +11,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    /// Restrict to landscape on iPhone; allow all orientations on iPad (for multitasking).
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return .all
+        }
+        return .landscape
+    }
+
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
